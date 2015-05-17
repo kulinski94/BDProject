2567b19744eafb02cab76e4a378ee914c7583b8b
ceco <ceco@ceko_pc>
2015-05-17 17:22:21 +0300

    hk

--- DBProject/src/main/java/com/dealers/db/dao/ProductDao.java
+++ DBProject/src/main/java/com/dealers/db/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.dealers.db.dao;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -121,7 +122,51 @@ public class ProductDao extends NamedParameterJdbcDaoSupport
 				int id = rs.getInt("id");
 				String name = rs.getString("name");
 				String photo = rs.getString("photo");
-				Integer count = rs.getInt("offersCount");
+				String category = rs.getString("category");
+				Product product = new Product(id, photo, name, Category
+						.valueOf(category));
+
+				ProductOffersStatistic stat = new ProductOffersStatistic();
+				stat.setCount(rs.getInt("offersCount"));
+				stat.setMaxPrice(rs.getBigDecimal("maxPrice"));
+				stat.setMinPrice(rs.getBigDecimal("minPrice"));
+				stat.setAveragePrice(rs.getBigDecimal("averagePrice"));
+				productsWithOfferCount.put(product, stat);
+
+			}
+
+		});
+
+		return productsWithOfferCount;
+	}
+	
+	public Map<Product, ProductOffersStatistic> getAllProductsWithOffersStatisticsWithPrice(
+			Category category, BigDecimal price)
+	{
+		String sql = ""
+				+ "SELECT "
+				+ "    id, name,photo,category, "
+				+ "		COUNT(dealer_id) AS offersCount,"
+				+ "		max(price) as maxPrice, "
+				+ " 	min(price)  as minPrice,"
+				+ "		avg(price) as averagePrice "
+				+ "FROM " + "    products " + "        LEFT JOIN "
+				+ "    offers ON id = product_id where category = :category "
+				+ "GROUP BY id HAVING price < :price";
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("category", category.name());
+		params.put("price", price);
+
+		Map<Product, ProductOffersStatistic> productsWithOfferCount = new HashMap<>();
+		getNamedParameterJdbcTemplate().query(sql, params,new RowCallbackHandler()
+		{
+
+			@Override
+			public void processRow(ResultSet rs) throws SQLException
+			{
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String photo = rs.getString("photo");
 				String category = rs.getString("category");
 				Product product = new Product(id, photo, name, Category
 						.valueOf(category));
